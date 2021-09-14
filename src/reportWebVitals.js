f4e9bbed8856e6374155f97d4c38e0583f4eefee
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-09-14 16:13:06 +0100

    fix: Fix linter error

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,19 +1,20 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(
+    import('web-vitals').then(
       ({
-      getCLS,
-      getFID,
-      getFCP,
-      getLCP,
-      getTTFB
-    }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+        getCLS,
+        getFID,
+        getFCP,
+        getLCP,
+        getTTFB,
+      }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      },
+    );
   }
 };
 
