ed55cb97fd238d955d868d8ae72f9a3646154143
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-09-22 16:39:11 +0100

    feat: Add 'qoutes.js'

---
+++ src/components/Quotes.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import styles from './Quotes.module.css';
+
+const Quotes = () => (
+  <div className={styles.main}>
+    <p>
+      "We will always have STEM with us. Some things will drop out of the public eye and go away, but there will always be science, engineering, and technology. And there will always, always be mathematics".
+      — Katherine Johnson, African-American mathematician
+    </p>
+
+  </div>
+);
+
+export default Quotes;
